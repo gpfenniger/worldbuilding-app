cc576891f595c54d41a3f3ef5ff4ad00349c8550
Griffin <pfenniger.griffin@gmail.com>
2019-01-07 17:21:02 +0000

    markdown file viewing

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const {app, BrowserWindow} = require('electron') 
 const url = require('url') 
 const path = require('path') 
+fs = require('fs')
 const {ipcMain} = require('electron')  
+const marked = require('marked')
 
 let win  
 
@@ -13,5 +15,19 @@ function createWindow() {
       slashes: true 
    })) 
 } 
-    
+
+// Event handler for asynchronous incoming messages
+ipcMain.on('read-file', (event, arg) => {
+   fs.readFile('files/test_page.md', 'utf8', function (err,data) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log("File Loaded");
+      event.sender.send('recieve-file', '<div>' + marked(data) + '</div>');
+      console.log("File sent to frontend");
+    });
+
+   console.log(arg)
+})
+
 app.on('ready', createWindow)
--- view.js
+++ view.js
@@ -1,1 +1,11 @@
 let $ = require('jquery')
+
+const {ipcRenderer} = require('electron')
+
+// Async message handler
+ipcRenderer.on('recieve-file', (event, arg) => {
+    $('#main-content').html(arg);
+})
+
+// Async message sender
+ipcRenderer.send('read-file', 'html')
